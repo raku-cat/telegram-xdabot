22dd92ab5a321d15dcb8b744d32084a28179c58b
Raku-cat <raku@rakutiki.tv>
2017-11-07 10:23:18 -0600

    add stuff easier

--- bot.py
+++ bot.py
@@ -10,6 +10,7 @@ from time import sleep
 from telepot.loop import MessageLoop
 from telepot.helper import InlineUserHandler, AnswererMixin
 from telepot.namedtuple import InlineQueryResultArticle, InputTextMessageContent
+from getpost import getpost
 
 with open(sys.path[0] + '/keys.json', 'r') as f:
     key = json.load(f)
@@ -18,6 +19,26 @@ bot = telepot.Bot(key['telegram'])
 if not os.path.exists('xda.txt'):
     with open('xda.txt', 'w') as f:
         f.read
+def on_command(msg):
+    content_type, chat_type, chat_id, msg_date, msg_id = telepot.glance(msg, long=True)
+    if content_type == 'text':
+        command = msg['text'].lower()
+        from_id = msg['from']['id']
+        try:
+            query = command.split(' ',1)[1]
+        except IndexError:
+            return
+        if command.startswith('/addpost'):
+            if from_id == 105301944:
+                bot.sendChatAction(chat_id, 'typing')
+                posttext = getpost(command)
+                if posttext is not None:
+                    with open('xda.txt', 'a') as xda:
+                        xda.write(posttext + '\n')
+                    bot.sendMessage(chat_id, 'Post added to model')
+                else:
+                    bot.sendMessage(chat_id, 'Invalid url')
+
 
 def on_inline_query(msg):
     query_id, from_id, query_string = telepot.glance(msg, flavor='inline_query')
@@ -46,7 +67,8 @@ def on_inline_query(msg):
         answerer.answer(msg, compute)
 
 answerer = telepot.helper.Answerer(bot)
-MessageLoop(bot, on_inline_query).run_as_thread()
+MessageLoop(bot,{'chat' : on_command,
+                  'inline_query' : on_inline_query}).run_as_thread()
 print('Started...')
 while 1:
     sleep(10)
---
+++ getpost.py
@@ -0,0 +1,25 @@
+import requests
+from urllib.parse import urlparse, parse_qs
+import json
+
+apibase = 'https://api.xda-developers.com/v1/posts/bypostid/'
+def getpost(posturl):
+    parsedurl = urlparse(posturl)
+    pq = parse_qs(parsedurl.query)
+    try:
+        postid = pq['p'][0]
+    except KeyError:
+        return
+    idparam = { 'postid' : postid }
+    post_request = requests.get(apibase, params=idparam)
+    if (post_request.status_code == requests.codes.ok):
+        try:
+            post_json = post_request.json()['results']
+            post = (post['pagetext'] for post in post_json if post['postid'] == postid).__next__()
+        except:
+            return
+        post = post.replace('\n','')
+        post = post.replace('\r','')
+        return post
+    else:
+        return
